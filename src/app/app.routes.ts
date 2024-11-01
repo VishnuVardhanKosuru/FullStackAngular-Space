bfdcdf0555e0e5f30b29243958f78a8fa09e4a26
Vishnu Vardhan Kosuru <amenu4cse21032@am.students.amrita.edu>
2024-11-01 22:09:33 +0530

    threejs bugs fixed
    issues and fixed bugs related to the future work field in spaceco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,19 @@ import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { InvestmentsComponent } from './components/investments/investments.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { CubeComponent } from './components/cube/cube.component';
+
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'signup',component:SignupComponent},
     {path:'aboutus',component:AboutusComponent},
-    {path:'investments',component:InvestmentsComponent}
+    {path:'investments',component:InvestmentsComponent},
+    {path:'gallery',component:GalleryComponent},
+    {path:'cube',component:CubeComponent}
+
 ];
+
+
